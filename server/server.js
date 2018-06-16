04608f03bc5041efc47989c7524e91802a054116
Alan Garza <alangarza@Alans-MacBook-Pro.local>
2018-06-15 22:56:07 -0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+
+    });
+}); 
 // Get /todos/131231
 
 app.listen(3000, () => {
