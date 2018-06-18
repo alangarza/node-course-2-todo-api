9218047aaf4fa4ceb087210f1757cf535349accc
Alan Garza <alangarza@Alans-MacBook-Pro.local>
2018-06-18 09:12:20 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo');
@@ -28,7 +29,49 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     });
 }); 
+
+
 // Get /todos/131231
+
+// app.get('/todos/:id', (req, res)=> {
+//     var id = req.params.id; 
+
+//     Todo.findById(id).then((todos)=> {
+//         if(todo)
+//         res.send({todos})
+//     }, (e) => {
+//         if (!ObjectID.isValid(id)){
+//             console.log('ID not valid');
+//             return res.status(400).send(e);
+//         } 
+//     });
+
+// });
+
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(400).send();
+        }
+
+        res.send({todo});
+    }).catch((e)=> {
+
+    });
+
+
+});
+
+
+
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
